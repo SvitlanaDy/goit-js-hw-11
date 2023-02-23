b5188fc517ed790262b7f3f5c001f457d65b3b64
Svitlana Dyshkant <110805508+SvitlanaDy@users.noreply.github.com>
2023-02-23 13:28:15 +0700

    upd1

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import Notiflix from 'notiflix';
+// const axios = require('axios/dist/node/axios.cjs');
+// Promise.resolve(1729).then(() => {
+//     console.log(`eid ${executionAsyncId()} tid ${triggerAsyncId()}`);
+//   });
+
+//   import { createHook, executionAsyncId, triggerAsyncId } from 'node:async_hooks';
+// createHook({ init() {} }).enable(); // forces PromiseHooks to be enabled.
+// Promise.resolve(1729).then(() => {
+//   console.log(`eid ${executionAsyncId()} tid ${triggerAsyncId()}`);
+// });
+// produces:
+// eid 7 tid 6
+
+// const fetchUsers = async () => {
+//     const response = await fetch("https://jsonplaceholder.typicode.com/users");
+//     const users = await response.json();
+//     return users;
+//   };
+  
+//   fetchUsers().then(users => console.log(users));
